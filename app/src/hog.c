a7f4aa50f00b900f83a1ba2b61967100d255f0da
Cem Aksoylar <caksoylar@users.noreply.github.com>
2023-11-15 21:11:41 -0800

    feat(mouse): Add mouse move and scroll support [WIP]
    Co-authored-by: Alexander Krikun <krikun98@gmail.com>
    Co-authored-by: Robert U <urob@users.noreply.github.com>
    Co-authored-by: Shawn Meier <ftc@users.noreply.github.com>
    Co-authored-by: Seth Milliken <seth@araxia.net>

--- app/src/hog.c
+++ app/src/hog.c
@@ -375,6 +375,8 @@ void send_mouse_report_callback(struct k_work *work) {
 
 K_WORK_DEFINE(hog_mouse_work, send_mouse_report_callback);
 
+K_WORK_DEFINE(hog_mouse_work, send_mouse_report_callback);
+
 int zmk_hog_send_mouse_report(struct zmk_hid_mouse_report_body *report) {
     int err = k_msgq_put(&zmk_hog_mouse_msgq, report, K_MSEC(100));
     if (err) {
@@ -395,7 +397,6 @@ int zmk_hog_send_mouse_report(struct zmk_hid_mouse_report_body *report) {
 
     return 0;
 };
-
 #endif // IS_ENABLED(CONFIG_ZMK_MOUSE)
 
 int zmk_hog_init(const struct device *_arg) {
